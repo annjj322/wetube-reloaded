96c65548c0d30e93d8c015cbc2aa09967f0eea49
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-10-27 13:46:39 +0900

    External Middlewares

--- src/server.js
+++ src/server.js
@@ -1,29 +1,21 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   console.log("I will respond.");
   return res.send("Hello");
 };
 
-const routerLogger = (req, res, next) => {
-  console.log("PATH", req.path);
-  next();
-};
-
-const middleWareLogger = (req, res, next) => {
-  console.log("METHOD", req.method);
-  next();
-};
-
 const handleLogin = (req, res) => {
   return res.send("Here Is A Login Page!");
 };
 
-app.use(middleWareLogger, routerLogger);
+app.use(logger);
 app.get("/", handleHome);
 app.get("/login", handleLogin);
 
