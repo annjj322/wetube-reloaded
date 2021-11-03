724cf3ff29dc05677e66fd5f5869765c95db8577
annjj322 <87896157+annjj322@users.noreply.github.com>
2021-11-03 21:15:12 +0900

    Conditionals, Iteration and Mixins

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,18 +1,46 @@
-export const trending = (req, res) => res.render("home", {pageTitle: "Home"});
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views: 59,
+      id: 1,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 // express -> view directory -> pug file 따로 import 해주지않아도 됨.
 
 export const see = (req, res) => {
   console.log(req.params);
-  return res.render("watch");
+  return res.render("watch", { pageTitle: "Watch" });
 };
 
 export const edit = (req, res) => {
-  return res.render("edit");
+  return res.render("edit", { pageTitle: "Edit" });
 };
 
 export const search = (req, res) => res.sent("Search");
 
-export const upload = (req, res) => res.send("Upload");
+export const upload = (req, res) => res.render("upload");
 
 export const deleteVideo = (req, res) => {
   return res.send("Delete");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,7 +3,7 @@ import { edit, remove, logout, see } from "../controllers/userController";
 
 const userRouter = express.Router();
 
-userRouter.get(":id", see);
+userRouter.get("/:id", see);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
 userRouter.get("/logout", logout);
